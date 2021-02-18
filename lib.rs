1791cdd4afeb9effee362d1593b3ee2c713d2e39
Ian Purton <ian.purton@gmail.com>
2021-02-18 08:29:28 +0000

    Add provider functionality.

--- lib.rs
+++ lib.rs
@@ -5,67 +5,66 @@ use ink_lang as ink;
 #[ink::contract]
 mod negotiate {
 
+    use ink_storage::{
+        collections::{
+            HashMap as StorageHashMap,
+            Vec as StorageVec,
+        },
+        traits::{
+            PackedLayout,
+            SpreadLayout,
+        },
+        Lazy,
+    };
+
+    #[derive(scale::Encode, scale::Decode, SpreadLayout, PackedLayout)]
+    #[cfg_attr(
+        feature = "std",
+        derive(
+            Debug,
+            PartialEq,
+            Eq,
+            scale_info::TypeInfo,
+            ink_storage::traits::StorageLayout
+        )
+    )]
+    pub struct ConjunctionDataMessage {
+        pub object1_norad_id: i32,
+        pub object2_norad_id: i32,
+        pub collision_probabilty: i32,
+        pub time_of_closest_pass: i32
+    }
+
     /// Defines the storage of your contract.
     /// Add new fields to the below struct in order
     /// to add new static storage fields to your contract.
     #[ink(storage)]
     pub struct Negotiate {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+        cdms: StorageVec<ConjunctionDataMessage>,
+        ca_providers: StorageVec<AccountId>
     }
 
     impl Negotiate {
         /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new() -> Self {
+            Self { 
+                cdms: Default::default(), 
+                ca_providers: Default::default(), 
+            }
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        pub fn add_ca_provider(&mut self, provider_account: AccountId) {
+            // Only the owner of this smart contract can call this method.
+            assert_eq!(self.env().caller(), self.env().account_id());
+
+            // Add this account to our list of roviders
+            self.ca_providers.push(provider_account);
         }
 
-        /// Simply returns the current value of our `bool`.
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
-        }
-    }
-
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// We test if the default constructor does its job.
-        #[test]
-        fn default_works() {
-            let negotiate = Negotiate::default();
-            assert_eq!(negotiate.get(), false);
-        }
-
-        /// We test a simple use case of our contract.
-        #[test]
-        fn it_works() {
-            let mut negotiate = Negotiate::new(false);
-            assert_eq!(negotiate.get(), false);
-            negotiate.flip();
-            assert_eq!(negotiate.get(), true);
+        pub fn submit_cdm(&mut self, cdm: ConjunctionDataMessage) {
         }
     }
 }
